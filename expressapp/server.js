710a916ded4c4f89306fb4c38da6eff61ef7d92c
Rachna <rachna.wasnik@gmail.com>
2020-04-09 15:43:58 +0530

    upload files on heroku

---
+++ expressapp/server.js
@@ -0,0 +1,11 @@
+const expres  = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname +'/dist/LoginRegistration'));
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname+'/dist/LoginRegistration/index.html'));
+
+}
+);
+
+app.listen(process.env.PORT || 8080);
